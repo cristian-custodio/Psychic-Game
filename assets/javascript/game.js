ebc1173e52745df5cf91b613c5847b2c22eaea2a
Cristian Custodio <cristian325@gmail.com>
2018-05-21 10:54:08 -0400

    Added HTML/CSS and JS Files

---
+++ assets/javascript/game.js
@@ -0,0 +1,124 @@
+
+var hiddenLetter = "";
+
+//Random Declaration
+var randomNumber = Math.floor(Math.random() * 27);
+console.log(randomNumber);
+
+//This Function is ran whenever the user presses a key
+document.onkeyup = function(event) {
+    //LowerCase user entry
+    var userInput = event.key.toLowerCase();
+
+
+
+}
+
+
+function resetGame() {
+    
+}
+
+function checkUserinput() {
+
+}
+
+function updateScore() {
+
+}
+
+function beginGame() {
+    
+
+
+    console.log(randomNumber);
+}
+
+function generateRandomLetter() {
+    var randomLetter = "";
+    var randomNumber = Math.floor(Math.random() * 27);
+    
+    switch (randomNumber) {
+        case 0:
+        randomLetter = a
+        break;
+        case 1:
+        randomLetter = b
+        break;
+        case 2:
+        randomLetter = c
+        break;
+        case 3:
+        randomLetter = d
+        break;
+        case 4:
+        randomLetter = e
+        break;
+        case 5:
+        randomLetter = f
+        break;
+        case 6:
+        randomLetter = g
+        break;
+        case 7:
+        randomLetter = h
+        break;
+        case 8:
+        randomLetter = i
+        break;
+        case 9:
+        randomLetter = a
+        break;
+        case 10:
+        randomLetter = a
+        break;
+        case 11:
+        randomLetter = a
+        break;
+        case 12:
+        randomLetter = a
+        break;
+        case 13:
+        randomLetter = a
+        break;
+        case 14:
+        randomLetter = a
+        break;
+        case 15:
+        randomLetter = a
+        break;
+        case 16:
+        randomLetter = a
+        break;
+        case 17:
+        randomLetter = a
+        break;
+        case 18:
+        randomLetter = a
+        break;
+        case 19:
+        randomLetter = a
+        break;
+        case 20:
+        randomLetter = a
+        break;
+        case 21:
+        randomLetter = a
+        break;
+        case 22:
+        randomLetter = a
+        break;
+        case 23:
+        randomLetter = a
+        break;
+        case 24:
+        randomLetter = a
+        break;
+        case 25:
+        randomLetter = a
+        break;
+        case 26:
+        randomLetter = a
+        break;
+    }
+}
